0429925ea8c9a78ab44a76a2f1d68a3eb7876194
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-07-27 19:29:07 -0300

    fix: table example

--- js/table.js
+++ js/table.js
@@ -1,79 +1,84 @@
 const FormScript = {
-  _init() {
-    $("button").on("click", (e) => {
-      let url = $("#urlInput").val();
-      this._fetchData(url);
-    });
-  },
-
-  _fetchData(url) {
-    $.get(url)
-      .then((data) => data.filter((data) => data.id > 5))
-      .then((data) => {
-        if (data.length === 0) {
-          throw Error("Não existe ususário com id > 5");
-        }
-
-        let dataNew = [];
-        data.map((item) => {
-          data = {
-            id: item.id,
-            name: item.name,
-            username: item.username,
-            email: item.email,
-          };
-
-          dataNew.push(data);
+    _init() {
+        $("button").on("click", (e) => {
+            let url = $("#urlInput").val();
+            this._fetchData(url);
         });
+    },
 
-        return dataNew;
-      })
-      .then((data) => {
-        data.forEach((el) => {
-          $("tbody").append(`<tr></tr><td>${el.id}</td>
+    _fetchData(url) {
+        $.get(url)
+            .then((data) => data.filter((data) => data.id > 2))
+            .then((data) => {
+
+                if (data.length === 0) {
+                    throw Error("Nao existe usuário com esse parametro");
+                }
+
+                let dataNew = [];
+                data.map((item) => {
+                    data = {
+                        id: item.id,
+                        name: item.name,
+                        username: item.username,
+                        email: item.email,
+                    };
+
+                    dataNew.push(data);
+                });
+
+                return dataNew;
+            })
+            .then((data) => {
+                data.forEach((el) => {
+                    $("tbody").append(`<tr></tr><td>${el.id}</td>
                       <td>${el.name}</td>
                       <td>${el.username}</td>
                       <td>${el.email}</td></tr>`);
-        });
+                });
 
-        return data.length;
-      })
-      .done((data) => {
-        if (data > 0) {
-          Toast.toast({
-            heading: "Sucesso",
-            text: `Foram adicionados ${data} pessoas`,
-            icon: "success",
-            showHideTransition: "",
-            allowToastClose: true,
-            hideAfter: 3000,
-            stack: 5,
-            position: "top-right",
-            textAlign: "left",
-            loader: true,
-            loaderBg: "#236f00",
-            bgColor: "#49de2f",
-            textColor: "black",
-          });
-        }
-      })
-      .catch((Error) => {
-        Toast.toast({
-          heading: "Erro",
-          text: `${Error.message}`,
-          icon: "warning",
-          showHideTransition: "",
-          allowToastClose: true,
-          hideAfter: 3000,
-          stack: 5,
-          position: "top-right",
-          textAlign: "left",
-          loader: true,
-          loaderBg: "#ec1132",
-          bgColor: "#f15555",
-        });
-      });
-  },
+                return data.length;
+            })
+            .done((data) => {
+                if (data > 0) {
+                    Toast.toast({
+                        heading: 'Successo',
+                        text: `Foram adicionados ${data} usuários`,
+                        icon: 'success',
+                        position: 'top-right',
+                        loaderBg: "#2aba1a",
+                        bgColor: "#23a138",
+                        textColor: "white"
+                    })
+                }
+            })
+            .catch((Error) => {
+
+                if (Error.status === 404) {
+                    Toast.toast({
+                        heading: 'Error 404',
+                        text: `Não foi possível realizar a operacao`,
+                        icon: 'warning',
+                        position: 'top-right',
+                        loaderBg: "#e71a1a",
+                        bgColor: "#e71a1a",
+                        textColor: "white"
+                    })
+                } else {
+                    Toast.toast({
+                        heading: 'Error',
+                        text: `${Error.message}`,
+                        icon: 'warning',
+                        position: 'top-right',
+                        loaderBg: "#e71a1a",
+                        bgColor: "#e71a1a",
+                        textColor: "white"
+                    })
+                }
+
+
+            });
+    },
 };
 
 var Toast = $;
